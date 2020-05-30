1960347a7fcc0a025e58e3b0f0205831391b168a
Sgoldik <sgoldik@mail.ru>
2020-05-30 14:19:25 +0700

    0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Red-Eye
-Video library on Go
+Video-downloader on Go.
+The app only supports default HTML tags (`<video><source src="URL"></video>`)
+
+# Install
+1. Check [this](https://zwbetz.com/download-chromedriver-binary-and-add-to-your-path-for-automated-functional-testing/ "this") instruction
+2. Install go compiler
+3. Enter `go run main.go` in console
+4. Enter the url of the site where the video is located
+5. Wait for the video to load
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/sclevine/agouti"
+)
+
+func main() {
+	var url string
+	fmt.Print("Enter site url: ")
+	fmt.Scanf("%s\n", &url)
+	driver := agouti.ChromeDriver(
+		agouti.ChromeOptions("args", []string{"--headless", "--disable-gpu", "--no-sandbox"}),
+	)
+
+	if err := driver.Start(); err != nil {
+		log.Fatal("Failed to start driver:", err)
+	}
+
+	page, err := driver.NewPage()
+	if err != nil {
+		log.Fatal("Failed to open page:", err)
+	}
+
+	if err := page.Navigate(url); err != nil {
+		log.Fatal("Failed to navigate:", err)
+	}
+
+	vid, err := page.Find(`source`).Attribute("src")
+	log.Println(vid)
+
+	download(vid)
+
+	if err := driver.Stop(); err != nil {
+		log.Fatal("Failed to close pages and stop WebDriver:", err)
+	}
+}
+
+func download(url string) {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	var filename = getFileName(url)
+
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		out, err := os.Create(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+
+		io.Copy(out, res.Body)
+
+		fmt.Println("Saved file ", filename)
+	} else {
+		fmt.Println(filename, " already exist")
+	}
+}
+
+func getFileName(url string) string {
+	hasher := md5.New()
+	io.WriteString(hasher, url)
+
+	var hashStr = hex.EncodeToString(hasher.Sum(nil)) + ".mp4"
+
+	fmt.Printf("%#v", hashStr)
+	return hashStr
+}
